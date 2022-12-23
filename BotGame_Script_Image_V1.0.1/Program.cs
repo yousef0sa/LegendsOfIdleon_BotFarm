7458ed5718057205ad5dec3d450b08f6c4f91aee
Yousef <yosef0sa@gmail.com>
2022-12-23 14:07:30 +0300

    make first  Multi MatchTemplate.  in Game. Called : "Dead Cells" .

--- BotGame_Script_Image_V1.0.1/Program.cs
+++ BotGame_Script_Image_V1.0.1/Program.cs
@@ -1,8 +1,25 @@
 ﻿using OpenCvSharp;
 using ScriptImage;
 
+//handle Game Called "Dead Cells"
+var handle = WindowInfo.handleTitle("Dead Cells");
+
 while (true)
 {
+    //Take screen-shot of the game window.
+    using (var mainImage = WindowInfo.Capture(handle))
+    {
+        //make MatchTemplate
+        var image = ImgProcess.MatchTemplate(mainImage, @"Images\Img1.png", 0.9);
 
-    Console.WriteLine(Mouse.GetCursorPosition());
+        //draw the matching
+        image.DrawMultiRec();
+
+        //this fixes the memory leak
+        image.Dispose();
+
+        //show me the result
+        Cv2.ImShow("Window", mainImage);
+        Cv2.WaitKey(1);
+    }
 }
